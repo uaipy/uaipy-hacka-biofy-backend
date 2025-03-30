62990beba544e63beea187a794c798c23e0c41d8
André Luiz Vicente Silva <andre.silva@estudante.iftm.edu.br>
2025-03-30 11:02:33 -0300

    add profile

--- database.py
+++ database.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 load_dotenv() 
 
 # Z-API DATA
-DATABASE_URL =  os.getenv("DATABASE_URL")
+DATABASE_URL =  os.getenv("NEW_DATABASE_URL")
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
 Base = declarative_base()
